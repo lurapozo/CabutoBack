dad1413691902775a04f75a16b4f26f920245f11
Allison Brito <adbrito@espol.edu.ec>
2020-08-15 13:18:30 -0500

    creacion de tablas productos, categoria, establecimiento y usuario superadmin

--- administracion/admin.py
+++ administracion/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Empresa
+from .models import Empresa,Establecimiento,Categoria,Producto
+
 # Register your models here.
 admin.site.register(Empresa)
+admin.site.register(Establecimiento)
+admin.site.register(Categoria)
+admin.site.register(Producto)
--- administracion/models.py
+++ administracion/models.py
@@ -11,3 +11,39 @@ class Empresa(models.Model):
 
 def __str__(self):
 	return self.nombre
+
+
+class Establecimiento(models.Model):
+	id_establecimiento=models.AutoField(primary_key=True)
+	nombre=models.CharField(max_length=100)
+	direccion=models.CharField(max_length=100)
+	telefono=models.CharField(max_length=100)
+	latitud=models.FloatField()
+	longitud=models.FloatField()
+	encargado=models.CharField(max_length=100)
+	image=models.BinaryField()
+	estado=models.CharField(max_length=1)
+
+def __str__(self):
+	return self.nombre
+
+class Categoria(models.Model):
+	id_categoria=models.AutoField(primary_key=True)
+	nombre = models.CharField(max_length=100)
+	image=models.BinaryField()
+	id_establecimiento=models.ForeignKey(Establecimiento,on_delete=models.SET_NULL, null=True)
+
+def __str__(self):
+	return self.nombre
+
+class Producto(models.Model):
+	id_producto = models.AutoField(primary_key=True)
+	nombre=models.CharField(max_length=100)
+	descripcion=models.CharField(max_length=100)
+	precio=models.FloatField()
+	image=models.BinaryField()
+	estado=models.CharField(max_length=1)
+	id_categoria=models.ForeignKey(Categoria,on_delete=models.SET_NULL, null=True)
+
+def __str__(self):
+	return self.nombre
